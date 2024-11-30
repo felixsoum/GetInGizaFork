cdbe5fc687f30cac7f97e408a80117badad659c2
Martinio91 <martin.samm20@hotmail.com>
2024-11-30 16:36:50 +0000

    Added background that moves with the camera

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform cameraTransform; // Reference to the camera
+    [SerializeField] private Vector2 parallaxEffectMultiplier = new Vector2(0.5f, 0.5f); // Adjust for speed
+
+    private Vector3 lastCameraPosition;
+
     void Start()
     {
-        
+        if (cameraTransform == null)
+        {
+            cameraTransform = Camera.main.transform; // Default to Main Camera
+        }
+        lastCameraPosition = cameraTransform.position;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x,
+                                          deltaMovement.y * parallaxEffectMultiplier.y,
+                                          0);
+        lastCameraPosition = cameraTransform.position;
     }
 }
